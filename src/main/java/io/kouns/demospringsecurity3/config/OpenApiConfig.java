8b464a2499bb7d1bc958a0b453ca776731f9fd6d
Herval NOUMONVI <herval80@gmail.com>
2023-10-13 10:04:45 +0100

    Configure Swagger to include JWT

---
+++ src/main/java/io/kouns/demospringsecurity3/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package io.kouns.demospringsecurity3.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info().title("REST API FOR BACKEND.")
+                        .description("This page show all ressources that you could handle to communicate with database via REST API.")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
--- src/main/java/io/kouns/demospringsecurity3/services/AuthService.java
+++ src/main/java/io/kouns/demospringsecurity3/services/AuthService.java
@@ -7,4 +7,6 @@ import org.springframework.http.ResponseEntity;
 public interface AuthService {
 
     ResponseEntity<?>  authenticateUser(LoginRequest loginRequest);
+
+    ResponseEntity<?> getUser();
 }
